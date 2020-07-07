e39415f1b8c28829a0947539960d8bf28146f5de
luis bento <lfbento@gmail.com>
2020-07-07 13:56:01 -0300

    Criando Controller

--- src/main/java/com/spring/codeblog/utils/DummyData.java
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -18,7 +18,7 @@ public class DummyData {
     CodeblogRepository codeblogRepository;
 
 
-    @PostConstruct
+    //@PostConstruct
     public void savePosts(){
 
         List<Post> postList = new ArrayList<>();
